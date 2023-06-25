18acef7b06cbd157095d7dc3fb7c812414f14665
Smile_简柠 <2646392644@qq.com>
2023-06-25 16:34:26 +0800

    add init project

---
+++ src/main/java/tyut/homework/webchat/WebChatApplication.java
@@ -0,0 +1,13 @@
+package tyut.homework.webchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tyut/homework/webchat/WebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package tyut.homework.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
