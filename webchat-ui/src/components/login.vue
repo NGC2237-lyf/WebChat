4d1d75a8be408fc70d14a4dbe179ca56cf54d5f8
侯瑞宁 <94113626+xiaomougui@users.noreply.github.com>
2023-06-27 21:17:01 +0800

    Feat:register

--- webchat-ui/src/components/login.vue
+++ webchat-ui/src/components/login.vue
@@ -8,23 +8,81 @@
       @mouseenter="mouseIn($event)"
       @mouseleave="mouseOut($event)"
     >
-      <h1 class="">Welcome to WebChat</h1>
+      <div class="font">
+        <h2>Welcome to WebChat</h2>
+      </div>
       <form action="">
-        <input class="tbx" type="text" placeholder="账号" @focus="" @blur="" />
-        <input class="tbx" type="password" placeholder="密码" />
+        <input
+          class="tbx"
+          type="text"
+          placeholder="账号"
+          @focus="closeOutControl"
+          @blur="openOutControl"
+        />
+        <input
+          class="tbx"
+          type="password"
+          placeholder="密码"
+          @focus="closeOutControl"
+          @blur="openOutControl"
+        />
         <input type="submit" value="登录" class="sub" />
-        <input type="submit" value="注册" class="sub" />
+        <input type="button" value="注册" class="sub" @click="gotoRegister" />
       </form>
       <span
         v-if="isAppear"
         :style="{
           top: position.top + 'px',
           left: position.left + 'px',
-          backgroundColor: currentColor,
+          backgroundColor: currentBgColor,
           color: fontColor,
         }"
         :class="{ in: !isIn, out: !isOut }"
       ></span>
+      <div id="register" :style="{ right: currentRegister }">
+        <div id="return">
+          <svg
+            t="1687869690461"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="2266"
+            width="20"
+            height="20"
+            @click="gotoLogin"
+          >
+            <path
+              d="M857.70558 495.009024 397.943314 27.513634c-7.132444-7.251148-18.794042-7.350408-26.048259-0.216941-7.253194 7.132444-7.350408 18.795065-0.216941 26.048259l446.952518 454.470749L365.856525 960.591855c-7.192819 7.192819-7.192819 18.85544 0 26.048259 3.596921 3.596921 8.311293 5.39487 13.024641 5.39487s9.42772-1.798972 13.024641-5.39487L857.596086 520.949836C864.747973 513.797949 864.796068 502.219239 857.70558 495.009024z"
+              fill="#1296db"
+              p-id="2267"
+            ></path>
+          </svg>
+        </div>
+        <div class="register-input">
+          <div class="login_box">
+            <!-- required就是不能为空 必须在css效果中有很大的作用 -->
+            <!-- 可以简写为required -->
+            <input type="text" required /><label>昵称</label>
+          </div>
+          <div class="login_box">
+            <input type="password" required="required" /><label>密码</label>
+          </div>
+          <div class="login_box">
+            <input type="password" required="required" /><label>性别</label>
+          </div>
+          <div class="login_box">
+            <input type="password" required="required" /><label>邮箱</label>
+          </div>
+          <a href="javascript:void(0)">
+            注册
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,8 +98,15 @@ export default {
     //是否可以退出
     let isOut = ref(false);
 
+    //函数执行控制器
+    let inControl = ref(true);
+    let outControl = ref(false);
+
     //是否显示
     let isAppear = ref(false);
+
+    //是否注册
+    let isRegister = ref(false);
 
     //span当前位置
     let position = reactive({
@@ -55,13 +120,19 @@ export default {
       g: 0,
       b: 0,
     });
+
     let currentBgColor = computed(() => {
       return `rgb(${color.r},${color.g},${color.b})`;
     });
     let fontColor = computed(() => {
       return `rgb(${255 - color.r},${255 - color.g},${255 - color.b})`;
     });
+    let currentRegister = computed(() => {
+      console.log(isRegister);
+      return isRegister.value ? `0%` : `-100%`;
+    });
 
+    //随时间变化的颜色
     function timeColor() {
       let date = new Date();
       let hours = date.getHours();
@@ -96,36 +167,27 @@ export default {
       }
     }
 
+    //鼠标进入
     function mouseIn(e) {
-      if (isIn) {
+      if (inControl.value) {
         //如果进去的门是打开的，就可以执行这个函数
-        console.log(e);
         //获取进入时的鼠标位置
         //生成元素的位置=进入点距离窗口的距离-父盒子距离窗口的距离
         position.left = e.clientX - e.target.offsetLeft;
         position.top = e.clientY - e.target.offsetTop;
-        // let inx = e.clientX - e.target.offsetLeft;
-        // let iny = e.clientY - e.target.offsetTop;
-
-        //创建一个span元素，给它对应的出生坐标
-        // let el = document.createElement("span");
-        // el.style.left = inx + "px";
-        // el.style.top = iny + "px";
-        // con.appendChild(el); //添加到con对应的元素，即container
-
-        // $(".container span").removeClass("out"); //移除出去的动画
-        // $(".container span").addClass("in"); //移除进入的动画
-        // span = document.querySelector(".container span");
         timeColor();
 
+        inControl.value = false;
+        outControl.value = true;
         isAppear.value = true;
         isIn.value = false; //关闭进来的门（不能使用进来的方法）
         isOut.value = true; //打开出去的门（可以使用出去的方法）
-        console.log(isAppear);
       }
     }
+
+    //鼠标退出
     function mouseOut(e) {
-      if (isOut) {
+      if (outControl.value) {
         //如果进去的门是打开的，就可以执行这个函数
 
         //获取进入时的鼠标位置
@@ -133,32 +195,56 @@ export default {
         position.left = e.clientX - e.target.offsetLeft;
         position.top = e.clientY - e.target.offsetTop;
 
-        // $(".container span").removeClass("in"); //移除进入的动画
-        // $(".container span").addClass("out"); //添加出去的动画
         timeColor();
-        // $(".out").css("left", inx + "px"); //添加出去的坐标
-        // $(".out").css("top", iny + "px");
 
+        outControl.value = false;
         isOut.value = false; //关闭出去的大门
 
         // 当动画结束后再删除元素
         setTimeout(() => {
           isAppear.value = false;
-          //   con.removeChild(span); //删除元素
+          inControl.value = true;
           isIn.value = true; //打开进来的大门
-          console.log(isAppear);
         }, 500);
       }
+    }
+
+    //鼠标退出可以触发退出动画
+    function openOutControl() {
+      outControl.value = true;
+      console.log(666);
+    }
+
+    //鼠标退出不可以触发退出动画
+    function closeOutControl() {
+      outControl.value = false;
+      console.log(777);
+    }
+
+    function gotoRegister() {
+      isRegister.value = true;
+    }
+
+    function gotoLogin() {
+      isRegister.value = false;
     }
     return {
       isIn,
       isOut,
+      inControl,
+      outControl,
       isAppear,
+      isRegister,
       currentBgColor,
+      currentRegister,
       fontColor,
       position,
       mouseIn,
       mouseOut,
+      gotoLogin,
+      gotoRegister,
+      openOutControl,
+      closeOutControl,
     };
   },
 };
@@ -216,7 +302,7 @@ source {
 
 .container {
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   flex-direction: column;
   width: 300px;
@@ -307,6 +393,52 @@ source {
   animation: out 0.5s ease-out forwards;
 }
 
+.font {
+  z-index: 1;
+  position: relative;
+}
+
+.font h2 {
+  overflow: hidden;
+  white-space: nowrap;
+  letter-spacing: 1px;
+  font-size: 1.2em;
+  color: white;
+  animation: typing 2s steps(20);
+}
+
+.font::after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: #fff;
+  animation: blink 1.1s linear infinite;
+}
+
+@keyframes blink {
+  0%,
+  49% {
+    opacity: 1;
+  }
+  50%,
+  100% {
+    opacity: 0;
+  }
+}
+
+@keyframes typing {
+  0% {
+    width: 0;
+  }
+  100% {
+    width: 236px;
+  }
+}
+
 /* 设置鼠标进入时，元素的动画 */
 @keyframes in {
   /* 初始关键帧 */
@@ -334,6 +466,176 @@ source {
   100% {
     width: 0;
     height: 0;
+  }
+}
+
+#register {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: #fff;
+  right: -100%;
+  transition: all 1s;
+  z-index: 2;
+}
+
+#return {
+  margin: 15px 0 0 10px;
+}
+
+#return svg {
+  transform: rotateY(180deg);
+  transition: all 1s;
+}
+
+#return svg:hover {
+  transform: rotateY(0deg);
+}
+
+.register-input {
+  /* 弹性布局 让子元素称为弹性项目 */
+  display: flex;
+  /* 让弹性项目垂直排列 原理是改变弹性盒子的主轴方向 父元素就是弹性盒子 现在改变后的主轴方向是向下了 */
+  flex-direction: column;
+  /* 让弹性项目在交叉轴方向水平居中 现在主轴的方向是向下 交叉轴的方向是与主轴垂直 交叉轴的方向是向右 */
+  align-items: center;
+  width: 100%;
+  padding: 40px;
+  color: #409eff;
+}
+.register-input h2 {
+  color: #409eff;
+  margin-bottom: 30px;
+}
+.register-input .login_box {
+  /* 相对定位 */
+  position: relative;
+  width: 100%;
+}
+.register-input .login_box input {
+  /* 清除input框自带的边框和轮廓 */
+  outline: none;
+  border: none;
+  width: 100%;
+  padding: 10px 0;
+  margin-bottom: 30px;
+  color: #409eff;
+  font-size: 16px;
+  border-bottom: 2px solid #409eff;
+  /* 背景颜色为透明色 */
+  background-color: transparent;
+}
+.register-input .login_box label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 10px 0;
+  color: #409eff;
+  /* 这个属性的默认值是auto 默认是这个元素可以被点击 但是如果我们写了none 就是这个元素不能被点击 , 就好像它可见但是不能用 可望而不可即 */
+  /* 这个就是两者的区别 */
+  pointer-events: none;
+  /* 加个过渡 */
+  transition: all 0.5s;
+}
+/* :focus 选择器是当input获得焦点是触发的样式 + 是相邻兄弟选择器 去找与input相邻的兄弟label */
+/* :valid 选择器是判断input框的内容是否合法,如果合法会执行下面的属性代码,不合法就不会执行,我们刚开始写布局的时候给input框写了required 我们删掉看对比 当没有required的话input框的值就会被认为一直合法,所以一直都是下方的样式 ,但是密码不会,密码框内的值为空,那么这句话局不合法,required不能为空 当我们给密码框写点东西的时候才会执行以下代码*/
+.register-input .login_box input:focus + label,
+.register-input .login_box input:valid + label {
+  top: -20px;
+  color: #409eff;
+  font-size: 12px;
+}
+.register-input a {
+  overflow: hidden;
+  position: relative;
+  padding: 10px 20px;
+  color: #409eff;
+  /* 取消a表现原有的下划线 */
+  text-decoration: none;
+  /* 同样加个过渡 */
+  transition: all 0.5s;
+}
+.register-input a:hover {
+  color: #fff;
+  border-radius: 5px;
+  background-color: #409eff;
+  box-shadow: 0 0 5px #409eff, 0 0 25px #409eff, 0 0 50px #409eff,
+    0 0 100px #409eff;
+}
+.register-input a span {
+  position: absolute;
+}
+.register-input a span:first-child {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  /* to right 就是往右边 下面的同理 */
+  background: linear-gradient(to right, transparent, #409eff);
+  /* 动画 名称 时长 linear是匀速运动 infinite是无限次运动 */
+  animation: move1 1s linear infinite;
+}
+.register-input a span:nth-child(2) {
+  right: 0;
+  top: -100%;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(transparent, #409eff);
+  /* 这里多了个0.25s其实是延迟时间 */
+  animation: move2 1s linear 0.25s infinite;
+}
+.register-input a span:nth-child(3) {
+  right: -100%;
+  bottom: 0;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(to left, transparent, #409eff);
+  animation: move3 1s linear 0.5s infinite;
+}
+.register-input a span:last-child {
+  left: 0;
+  bottom: -100%;
+  width: 2px;
+  height: 100%;
+
+  background: linear-gradient(#409eff, transparent);
+  animation: move4 1s linear 0.75s infinite;
+}
+/* 写一下动画 再坚持一下 视频马上就完了 */
+@keyframes move1 {
+  0% {
+    left: -100%;
+  }
+  50%,
+  100% {
+    left: 100%;
+  }
+}
+@keyframes move2 {
+  0% {
+    top: -100%;
+  }
+  50%,
+  100% {
+    top: 100%;
+  }
+}
+@keyframes move3 {
+  0% {
+    right: -100%;
+  }
+  50%,
+  100% {
+    right: 100%;
+  }
+}
+@keyframes move4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,
+  100% {
+    bottom: 100%;
   }
 }
 </style>
--- webchat-ui/vue.config.js
+++ webchat-ui/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false, //关闭语法检查
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './'  //打包优化
 })
