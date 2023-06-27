c6bd981668fd8745f8b0a9472ce31a4729916349
Smile_简柠 <2646392644@qq.com>
2023-06-27 08:20:58 +0800

    add projct domain

---
+++ src/main/java/tyut/homework/webchat/common/utils/Result.java
@@ -0,0 +1,44 @@
+package tyut.homework.webchat.common.utils;
+
+public class Result {
+    private int code;//状态码
+
+    private String msg;//提示信息
+
+    private Object data;//数据
+
+    public Result() {
+    }
+
+    public Result(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public Result(int code, Object data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public Result(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static Result success(String msg){
+        return new Result(200,msg);
+    }
+
+    public static Result success(Object obj){
+        return new Result(200,obj);
+    }
+
+    public static Result error(String msg){
+        return new Result(500,msg);
+    }
+
+    public static Result error(Object obj){
+        return new Result(500,obj);
+    }
+}
