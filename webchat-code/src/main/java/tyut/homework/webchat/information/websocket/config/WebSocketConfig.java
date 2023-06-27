ad8431c7c1a6e34c0a1f856e3e90f84c702d4406
TokisakiKurumi <2105721136@qq.com>
2023-06-27 18:01:02 +0800

    feat:添加基本类

---
+++ webchat-code/src/main/java/tyut/homework/webchat/information/websocket/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package tyut.homework.webchat.information.websocket.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import tyut.homework.webchat.information.websocket.handler.ChatHandler;
+import tyut.homework.webchat.information.websocket.interceptor.WebSocketInterceptor;
+
+/**
+ * @author TokisakiKurumi
+ * @className WebSocketConfig
+ * @date 2023/6/27
+ * @description
+ **/
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler,"/chat")
+                .addInterceptors(new WebSocketInterceptor())
+                .setAllowedOrigins("*");
+    }
+}
