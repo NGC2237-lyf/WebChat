aa4c65c3196aeed9776df6261065bce4b51c3dc1
侯瑞宁 <94113626+xiaomougui@users.noreply.github.com>
2023-07-04 13:47:28 +0800

    Feat:add chat

---
+++ webchat-ui/src/views/chat/screen.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="screen">
+    <Message v-for="item in message" :info="item" />
+  </div>
+</template>
+
+<script>
+import Message from "./message.vue";
+import { ref, reactive, onMounted, computed } from "vue";
+export default {
+  name: "screen",
+  props: ["message"],
+  components: {
+    Message,
+  },
+  setup() {
+    let person = ref(0);
+    let users = reactive([
+      {
+        id: 666,
+        name: "",
+        icon: "",
+      },
+    ]);
+    let words = reactive(["你好", ""]);
+    function getWords() {}
+    function generateWords() {}
+    return {
+      getWords,
+      generateWords,
+    };
+  },
+};
+</script>
+
+<style>
+.screen {
+  width: 100%;
+  height: 500px;
+  height: 70%;
+  border: 1px solid;
+}
+</style>
---
+++ webchat-ui/src/views/main/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <My />
+    <Firends />
+    <Chat />
+  </div>
+</template>
+
+<script>
+import My from "@/views/my/index.vue";
+import Chat from "@/views/chat/index.vue";
+import Firends from "@/views/firends/index.vue";
+import { ref, reactive, onMounted, computed } from "vue";
+export default {
+  name: "main",
+  components: {
+    Chat,
+    Firends,
+    My,
+  },
+  setup() {
+    onMounted(() => {});
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  width: 80%;
+  margin: 0 auto;
+  background-color: aquamarine;
+  height: 800px;
+  display: flex;
+}
+</style>
